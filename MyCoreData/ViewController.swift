fb2f5efff26c446e1049aa9a6ddc53e8beae5d30
Bodikov Kuat <kuat.bodikov@gmail.com>
2022-01-26 16:06:42 +0500

    Initial Commit

---
+++ MyCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyCoreData
+//
+//  Created by Kuat Bodikov on 26.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
